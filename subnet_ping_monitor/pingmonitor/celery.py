0cd4b77e9d6ab461eef8e366ae54497353fab2fd
kutayyildirim <127936555+kutayyildirim@users.noreply.github.com>
2025-07-11 14:49:42 +0300

    Delete subnet_ping_monitor directory

--- subnet_ping_monitor/pingmonitor/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pingmonitor.settings')
-
-app = Celery('pingmonitor')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
