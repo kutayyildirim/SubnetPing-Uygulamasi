8dbc38d6ba8c029975fe7771392ac7d0e218a649
kutayyildirim <127936555+kutayyildirim@users.noreply.github.com>
2025-07-21 17:13:07 +0300

    Delete subnet_ping_monitor directory

--- subnet_ping_monitor/netping/serializers/ping_result_serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from netping.models.ping_result import PingResult
-
-
-class PingResultSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PingResult
-        fields = ['ip_address', 'is_alive', 'response_time_ms', 'checked_at']
--- subnet_ping_monitor/netping/serializers/subnet_request_serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from netping.models.subnet_request import SubnetRequest
-
-
-class SubnetRequestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SubnetRequest
-        fields = ['id', 'ip_network', 'is_ipv6', 'status', 'created_at']
-        read_only_fields = ['id', 'status', 'created_at']
